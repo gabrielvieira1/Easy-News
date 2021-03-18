d35bd817bea5bea9fb8b59085a51268cba25f9bb
Gabriel Cezario <GabrielCezarioR@gmail.com>
2021-03-17 22:24:45 -0300

    created backend base

---
+++ backend/src/test/java/br/com/easynews/EasynewsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easynews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasynewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
