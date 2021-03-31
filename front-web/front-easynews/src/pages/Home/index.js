21d7a612c0abea375c4ddffca3f40d1474b052d0
Weza Bernardo <wezabrasil@outlook.com>
2021-03-30 23:20:21 -0300

    Styles Home

---
+++ front-web/front-easynews/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+import '../../pages/global.css';
+import image from '../../assets/Images/Nuvens.svg';
+function Home(){
+    return(
+        <div id="background">
+           
+                    <main>
+                        <h1>Easy News</h1>
+                        <p>Tornando seu caminho melhor com informações do dia-a-dia</p>
+                    </main>
+       
+
+        </div>
+    );
+}
+export default Home;
