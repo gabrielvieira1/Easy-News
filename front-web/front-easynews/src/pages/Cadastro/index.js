cd8d4dbb4bd361c58b9095477c2f31cee4359337
Gabriel Cezario <GabrielCezarioR@gmail.com>
2021-04-18 17:49:20 -0300

    integrando cadastro de usuário

--- front-web/front-easynews/src/pages/Cadastro/index.js
+++ front-web/front-easynews/src/pages/Cadastro/index.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import logoAmarelo from "../../assets/Images/LogoAmarelo.svg";
 import mapWoman from "../../assets/Images/mapWoman.svg";
 import TextField from "@material-ui/core/TextField";
+import { api } from '../../services/api';
+import { cadastrarUsuario } from '../../services/cadastroUsuarioService'
 
 import {
   Container,
@@ -21,60 +23,90 @@ import {
   Top,
 } from "./styles";
 
-function Cadastro() {
-  return (
-    <Container>
-      <Main>
-        <Left>
-          <Image src={logoAmarelo} alt="logo" width="353" />
-          <Slogan>Você está a um passo de definir o seu destino.</Slogan>
-          <Image src={mapWoman} alt="mulher usando mapa" width="650" />
-        </Left>
+export default class Cadastro extends React.Component{
 
-        <Right>
-          <Top>
-            <h1>Inscreva-se no Easy N3ws </h1>
-            <LinkGoogle href="/">Cadastre-se com o Google</LinkGoogle>
-          </Top>
-          <Form>
-            <Fieldset>
-              <div>
-                <Label> Nome </Label>
-                <Input
-                  type="text"
-                  name="nome"
-                  placeholder="Digite seu nome aqui"
-                />
-              </div>
-              <div>
-                <Label> Nome e usuário </Label>
-                <Input
-                  type="text"
-                  name="usuario"
-                  placeholder="Digite sua nome de usuario"
-                />
-              </div>
-            </Fieldset>
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      userName: '',
+      email: '',
+      password: ''
+    }
 
-            <FieldsetEmail>
-              <Label> E-mail </Label>
-              <Input
-                type="e-mail"
-                name="email"
-                placeholder="Digite seu e-mail"
-              />
-              <Label> Senha </Label>
-              <Input
-                type="password"
-                name="senha"
-                placeholder="Digite sua senha"
-              />
-            </FieldsetEmail>
-            <LinkCadastro href="/">Cadastrar</LinkCadastro>
-          </Form>
-        </Right>
-      </Main>
-    </Container>
-  );
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    cadastrarUsuario(this.state);
+  }
+
+  changeHandler = (event) => {
+    this.setState(
+      {[event.target.name]: event.target.value}
+    );
+  }
+
+  render(){
+    return (
+      <Container>
+        <Main>
+          <Left>
+            <Image src={logoAmarelo} alt="logo" width="353" />
+            <Slogan>Você está a um passo de definir o seu destino.</Slogan>
+            <Image src={mapWoman} alt="mulher usando mapa" width="650" />
+          </Left>
+  
+          <Right>
+            <Top>
+              <h1>Inscreva-se no Easy N3ws </h1>
+              <LinkGoogle href="/">Cadastre-se com o Google</LinkGoogle>
+            </Top>
+            <Form>
+              <Fieldset>
+                <div>
+                  <Label> Nome </Label>
+                  <Input
+                    type="text"
+                    name="name"
+                    placeholder="Digite seu nome aqui"
+                    onBlur={this.changeHandler}
+                  />
+                </div>
+                <div>
+                  <Label> Nome e usuário </Label>
+                  <Input
+                    type="text"
+                    name="userName"
+                    placeholder="Digite sua nome de usuario"
+                    onBlur={this.changeHandler}
+                  />
+                </div>
+              </Fieldset>
+  
+              <FieldsetEmail>
+                <Label> E-mail </Label>
+                <Input
+                  type="e-mail"
+                  name="email"
+                  placeholder="Digite seu e-mail"
+                  onBlur={this.changeHandler}
+                />
+                <Label> Senha </Label>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="Digite sua senha"
+                  onBlur={this.changeHandler}
+                />
+              </FieldsetEmail>
+              <LinkCadastro onClick= {this.submitHandler} >Cadastrar</LinkCadastro>
+            </Form>
+          </Right>
+        </Main>
+      </Container>
+    );
+  }
 }
-export default Cadastro;
