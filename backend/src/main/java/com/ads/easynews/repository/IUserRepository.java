68abf669f59e855c927c67c0c1fe908826e797c0
Gabriel Cezario <gabrielcezarior@gmail.com>
2021-05-15 20:20:14 -0300

    adicionando login de usuario

--- backend/src/main/java/com/ads/easynews/repository/IUserRepository.java
+++ backend/src/main/java/com/ads/easynews/repository/IUserRepository.java
@@ -1,9 +1,13 @@
 package com.ads.easynews.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ads.easynews.model.entities.User;
 
 public interface IUserRepository extends JpaRepository<User, Long>{
 
+	@Query("FROM User u WHERE u.email = ?1 and u.password = ?2")
+	User buscarUsuarioPorLogin(String email, String password);
+	
 }
